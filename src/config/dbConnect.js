c43b1fd55ef46b15d3251219b8ec58fc926e4918
Andre Rodrigues <andre23dev@gmail.com>
2023-05-08 19:57:05 -0300

    code review

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from "mongoose"
+import mongoose from "mongoose"
 
 mongoose.connect("mongodb+srv://applivraria:passlivraria@cluster-webapp-livraria.ygplyo8.mongodb.net")
 
